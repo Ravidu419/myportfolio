47ad93fdc8124219c24389c9c834412279c05c3e
ravidu <turavidu@gmail.com>
2025-11-28 21:12:42 +0530

    Fixed base url path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", 
+  base: "/myportfolio/", 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
